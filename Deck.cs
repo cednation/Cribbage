f118dd9b038f4f24f024223e03f7cc7f69a79f7a
Cedric Davies <cedricda@microsoft.com>
2022-11-26 14:00:02 -0800

    Initial Cribbage project
    Classes: Card, Deck, PlayerHand, HandScorer with unit tests. Proud that HandScorer worked. Tested on 24 hand, 29 hand, and flush

---
+++ Deck.cs
@@ -0,0 +1,36 @@
+﻿namespace Cribbage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    public class Deck
+    {
+        private readonly List<Card> cards;
+        private readonly Random random = new(DateTime.UtcNow.Microsecond);
+
+        public Deck()
+        {
+            this.cards = new List<Card>(capacity: 52);
+
+            foreach (Suit s in Enum.GetValues<Suit>())
+            {
+                foreach (Rank r in Enum.GetValues<Rank>())
+                {
+                    this.cards.Add(new Card(r, s));
+                }
+            }
+        }
+
+        public Card DealRandomCard()
+        {
+            int cardIndex = random.Next(this.cards.Count);
+            var randomCard = this.cards[cardIndex];
+            this.cards.RemoveAt(cardIndex);
+
+            return randomCard;
+        }
+
+        public ReadOnlyCollection<Card> Cards => this.cards.AsReadOnly();
+    }
+}
